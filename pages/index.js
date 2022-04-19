16971e9818e9e31184c402993fa5ea1abdac8f6e
Zach Lewitt <zach-l@hotmail.co.uk>
2022-04-19 17:26:54 +0100

    Remove template boilerplate from Readme and index

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Lewitts Wedding</title>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Lewitts Wedding 2023" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Coming soon...
         </p>
       </main>
 
