e12005e31df401fa6e27bd38f345754bdff75f4d
Zach Lewitt <zach-l@hotmail.co.uk>
2022-07-13 19:25:30 +0100

    Change casing on confirmation text

--- pages/absent-confirmation.jsx
+++ pages/absent-confirmation.jsx
@@ -3,7 +3,7 @@ import { Confirmation } from "@components/confirmation";
 export default function AbsentConfirmation() {
   return (
     <Confirmation
-      headingText="Thanks for letting us know"
+      headingText="Thanks For Letting Us Know"
       summaryText="Sorry to hear that you can't make it to the wedding. We'll have to celebrate back in the UK."
       linkText="Confirm Another Person"
     />
--- pages/attending-confirmation.jsx
+++ pages/attending-confirmation.jsx
@@ -3,7 +3,7 @@ import { Confirmation } from "@components/confirmation";
 export default function AttendingConfirmation() {
   return (
     <Confirmation
-      headingText="Thanks for confirming"
+      headingText="Thanks For Confirming"
       summaryText="We can't wait to see you there on the 1st August 2023!"
       linkText="Confirm Another Person"
     />
