5c4300d1c68e5db76745768354af48b73323a981
Zach Lewitt <zach-l@hotmail.co.uk>
2022-08-08 18:50:44 +0100

    Add link tag to favicon

--- pages/_document.js
+++ pages/_document.js
@@ -30,7 +30,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
         <body>
           <Main />
           <NextScript />
